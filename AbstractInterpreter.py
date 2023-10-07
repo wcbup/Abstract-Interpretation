896a836af2874ca225c82ed683c5f5d23bcf0b8e
wcbup <wcbup123@foxmail.com>
2023-10-07 22:45:34 +0200

    `__gt__` supports sign abstraction

--- AbstractInterpreter.py
+++ AbstractInterpreter.py
@@ -47,7 +47,7 @@ class AbstractType(Enum):
     NEGATIVE_INT = "Negative Int"  # negative int
     NOT_POSITIVE_INT = "Not Positive Int"  # not positive int
     NOT_NEGATIVE_INT = "Not Negative Int"  # not negative int
-    NOT_ZERO = "Not Zero" # not zero
+    NOT_ZERO = "Not Zero"  # not zero
 
 
 class ExceptionType(Enum):
@@ -191,7 +191,23 @@ class AbstractVariable:
             case _:
                 raise Exception
 
-    def __gt__(self, b: AbstractVariable) -> bool | None:
+    def __gt__(
+        self, b: AbstractVariable
+    ) -> (
+        bool
+        | None
+        | Tuple[
+            Tuple[AbstractVariable, AbstractVariable],
+            Tuple[AbstractVariable, AbstractVariable],
+        ]
+    ):
+        """
+        return bool if result is specific
+        return None if result is unkown
+        return Tuple with update variabls tuples,
+            the first tuple is the variables when result is true
+            the second tuple is the variables when result is false
+        """
         if not isinstance(b, AbstractVariable):
             raise Exception
         match self.type:
@@ -208,8 +224,31 @@ class AbstractVariable:
 
             case AbstractType.ANY_INT:
                 match b.type:
-                    case AbstractType.INT | AbstractType.ANY_INT:
+                    case AbstractType.ANY_INT:
                         return None
+
+                    case AbstractType.INT:
+                        match ABSTRACT_MODE:
+                            case AbstractMode.ANY_INT:
+                                return None
+
+                            case AbstractMode.SIGN:
+                                true_variables = (
+                                    AbstractVariable(AbstractType.POSITIVE_INT),
+                                    AbstractVariable(0),
+                                )
+                                true_variables[0].memory_id = self.memory_id
+                                true_variables[1].memory_id = b.memory_id
+                                false_variables = (
+                                    AbstractVariable(AbstractType.NOT_POSITIVE_INT),
+                                    AbstractVariable(0),
+                                )
+                                false_variables[0].memory_id = self.memory_id
+                                false_variables[1].memory_id = b.memory_id
+                                return true_variables, false_variables
+
+                            case _:
+                                raise Exception(ABSTRACT_MODE)
 
                     case _:
                         raise Exception
@@ -355,7 +394,7 @@ class AbstractVariable:
                                 false_variables[0].memory_id = self.memory_id
                                 false_variables[1].memory_id = b.memory_id
                                 return true_variables, false_variables
-                            
+
                             case _:
                                 raise Exception(ABSTRACT_MODE)
 
